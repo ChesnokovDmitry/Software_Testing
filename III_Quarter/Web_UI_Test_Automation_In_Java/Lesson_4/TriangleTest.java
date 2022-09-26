1aa26bb71412cdedd96237d1efe07db9b47cc803
Dmitry Chesnokov <dmitrijches@gmail.com>
2022-09-26 22:06:07 +0300

    added Web_UI_Automation_Test_In_Java_Lesson_4

---
+++ III_Quarter/Web_UI_Test_Automation_In_Java/Lesson_4/TriangleTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class TriangleTest {
+
+    @ParameterizedTest
+    @CsvSource({"3, 5, 6", "0, 5, 6"})
+    public void nullCheckTest(int a, int b, int c) {
+        Assertions.assertTrue(Triangle.isNull(a, b, c));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3, 5, 6", "-1, 5, 6"})
+    public void negativeNumberTest(int a, int b, int c) {
+        Assertions.assertTrue(Triangle.isNegativeNumber(a, b, c));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3, 5, 6", "4, 5, 6"})
+    public void areaCalculationCheckTest(int a, int b, int c) {
+        double s = 7.483314773547883;
+        Assertions.assertEquals(s, Triangle.areaOfATriangle(a, b, c));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3, 5, 6", "100, 5, 6"})
+    public void incorrectValueTest(int a, int b, int c) {
+        Assertions.assertTrue(Triangle.isSumOfSides(a, b, c));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"3, 5, 6", "0, 5, 6", "-1, 5, 6", "100, 5, 6"})
+    public void exceptionTest(int a, int b, int c) {
+        Assertions.assertThrows(RuntimeException.class, () -> Triangle.areaOfATriangle(a, b, c));
+    }
+}
